b7fae31c85da3e3f9695e241bcff6737cec337e1
Kendall <ktrudickm@gmail.com>
2021-02-09 17:45:17 -0500

    Populated while loop into array and converted array to a string to make the password. Still need to display password, and having trouble getting it to be the specific number of string length

--- Develop/script.js
+++ Develop/script.js
@@ -16,30 +16,61 @@ function generatePassword() {
   var passLength = false;
 
   while (!passLength) {
-    var length = parseInt(prompt("How many characters would you like your password (between 8 and 128 characters)?"));
-    if (!length) {
+    var passwordLength = parseInt(prompt("How many characters would you like your password (between 8 and 128 characters)?"));
+    if (!passwordLength) {
     alert("You must enter a number.");
-    length = parseInt(prompt("How many characters would you like your password (between 8 and 128 characters)?"));
+    passwordLength = parseInt(prompt("How many characters would you like your password (between 8 and 128 characters)?"));
   }
-    else if (length < 8 || length > 128 || length === 0) {
+    else if (passwordLength < 8 || passwordLength > 128 || passwordLength === 0) {
     alert("Value must be between 8 and 128.");
-    length = parseInt(prompt("How many characters would you like your password (between 8 and 128 characters)?"));
+    passwordLength = parseInt(prompt("How many characters would you like your password (between 8 and 128 characters)?"));
   }
     else {
     passLength = true;
   }
 }
 
-
-
-
+  // Creating the array for the numbers
+  var Numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
   numbConfirm = confirm("Will this contain numbers?");
+
+  // Creating array for the characters
+  var characters = ["!", "@", "#", "$", "%", "^", "&", "*", "?", "+", "="];
   charConfirm = confirm("Will this contain special characters?");
+
+  //Creating the array for the Uppercase Letters
   upperConfirm = confirm("Will this contain uppercase letters?");
+  var uppercase = ["A", "B", "C", "D", "E", "F","G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+
+  //Creating array for lowercase letters
   lowerConfirm = confirm("Will this contain lowercase letters?");
+  var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
+  var output = [""];
 
+  while (output.length < passwordLength) {
+
+    if (numbConfirm === true) {
+      output.push(Numbers[Math.floor(Math.random() * Numbers.length)]);
+    }
+
+    if (charConfirm === true) {
+      output.push(characters[Math.floor(Math.random() * characters.length)]);
+    }
+
+    if (upperConfirm === true) {
+      output.push(uppercase[Math.floor(Math.random() * uppercase.length)]);
+    }
+
+    if (lowerConfirm === true) {
+      output.push(lowercase[Math.floor(Math.random() * lowercase.length)]);
+    }
+  }
+var password = output.toString();
+console.log(password);
 }
+
+
 
 // Write password to the #password input
 function writePassword() {
